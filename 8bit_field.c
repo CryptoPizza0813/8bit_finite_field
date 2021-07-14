1692dfc856726676fafb1f50162cbb3a1141e477
jhdh0813 <56108428+jhdh0813@users.noreply.github.com>
2021-07-14 23:22:27 +0900

    0714

--- 8bit_field.c
+++ 8bit_field.c
@@ -5,7 +5,8 @@
 BYTE field_addition(BYTE A, BYTE B);        // return A + B
 BYTE field_multiplication(BYTE A, BYTE B);  // return A * B mod f(x)
 BYTE field_division(BYTE A, BYTE B);        // return A / B
-BYTE field_inverse(BYTE A);                 // return A^{-1} in GF(2^8)
+BYTE field_inverse(BYTE h);                 // return A^{-1} in GF(2^8)
+int find_deg(BYTE A);
 
 int main()
 {
@@ -14,14 +15,9 @@ int main()
     return 0; 
     */
 
-    BYTE A = 0x11;
-    BYTE B = 0x01;
-    BYTE C = 0xf9;
+    BYTE A = 0x53;
 
-    BYTE D = field_multiplication(A,B);
-    printf("%02x\n", D);
-    D = field_multiplication(A,C);
-    printf("%02x\n", D);
+    printf("%02x\n", field_inverse(A));
 
 
     return 0;
@@ -45,7 +41,7 @@ BYTE field_multiplication(BYTE A, BYTE B)
         // 매 루프마다 A에 x를 곱함(<<1). A의 최고차항이 7이면 carry가 발생하므로 0x1b(기약다항식)을 XOR 한다.
         // 즉, A * x^i를 미리 계산 
         if(A & 0x80)
-            A = (A<<1)^0x1B;
+            A = (A<<1)^0x1b;
         else
             A <<= 1;
         mask <<= 1;   
@@ -57,40 +53,99 @@ BYTE field_multiplication(BYTE A, BYTE B)
 // return (A // B)
 BYTE field_division(BYTE A, BYTE B) 
 {
-    if (A < B)
-        return 0x00;
-    else if (A == B)
-        return 0x01;
+    int deg_A, deg_B;
+    BYTE quotient = 0x00, temp = 0x00;
 
-    // A 최고차항 차수 찾기
-    BYTE mask = 0x08;
-    int deg_A = 7;  // A 차수를 저장할 변수
+    while(1) {
+        BYTE mask = 0x01;
+        deg_A = find_deg(A);
+        deg_B = find_deg(B);
+
+        if(deg_A < deg_B)
+            break;
+        
+        A ^= (B << (deg_A - deg_B));
+        quotient ^= (mask << (deg_A - deg_B));
+    }
+
+    return quotient;
+}
+
+
+
+BYTE field_inverse(BYTE h)  // A = g(x)
+{
+    if(h == 0)
+        return 0;
+
+    BYTE g = 0x1b;  // 기약다항식
+
+    int deg_h = find_deg(h);
+    g ^= (h << (8-deg_h));
+    BYTE init_quotient = (0x01 << (8-deg_h)), quotient;
+
+    // printf("%02x \t %02x\n", g, init_quotient);
+
+
+
+    BYTE t2 = 0, t1 = 1, r, t, temp;
+    int flag = 0;
+
+    int round = 0;
+    while(h != 0) {
+        printf("round = %d\n", round);
+        round += 1;
+
+        // q = g div h
+        quotient = field_division(g, h);
+        if(flag == 0) {
+            quotient ^= init_quotient;
+            g = 0x1b;
+            flag += 1;
+        }
+
+
+        // r = g - h * q
+        temp = field_multiplication(h, quotient);
+        r = field_addition(g, temp);
+
+
+        // t <- t2 - q * t1
+        temp = field_multiplication(quotient, t1);
+        t = field_addition(t2, temp);
+
+        // g <- h, h <- r
+        g = h;
+        h = r;
+
+
+        // t2 <- t1, t1 <- t
+        t2 = t1;
+        t1 = t;
+    }
+
+    // t <- t2
+    t = t2;
+    printf("answer = t = %02x\n", t);
+
+    return t;
+}
+
+
+int find_deg(BYTE A)
+{
+    BYTE mask = 0x80;
+    int deg = 7;
+
     for(int i = 0; i < 8; i++) {
         if(A & mask)
             break;
         else {
-            mask >> 01;
+            deg -= 1;
+            mask >>= 1;
         }
 
-
-
     }
 
-
-    while(A > B) {
-
-
-
-    }
-
+    return deg;
 }
-
-/*
-BYTE field_inverse(BYTE A)  // A = g(x)
-{
-
-let g = 0x11b, h = A
-
-
-}
-*/
